1ad1316d269776ca878c8b9278d70cf4b1ff4e40
Michael Jasper <mjasper@instructure.com>
2018-10-16 13:55:56 -0600

    2nd initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,9 @@
 #![feature(const_vec_new)]
 
-extern crate js_sys;
 extern crate wasm_bindgen;
-extern crate web_sys;
-extern crate console_error_panic_hook;
 
 use wasm_bindgen::prelude::*;
 
-// #[wasm_bindgen]
-// pub fn main(rgba: &[i32]) -> Vec<i32>{
-//   let len: i32 = rgba.len() as i32;
-//   let mut rgba_v = Vec::new();
-//   for x in (1..len).step_by(4) {
-//     let r = rgba[x as usize];
-//     let g = rgba[(x+1) as usize];
-//     let b = rgba[(x+2) as usize];
-//     let average = (r+b+g)/3;
-//     rgba_v.push(average);
-//     rgba_v.push(average);
-//     rgba_v.push(average);
-//     rgba_v.push(255);
-//   }
-//   rgba_v
-// }
-
-// #[wasm_bindgen]
-// extern "C" {
-//     // Use `js_namespace` here to bind `console.log(..)` instead of just
-//     // `log(..)`
-//     #[wasm_bindgen(js_namespace = console)]
-//     fn log(s: &str);
-
-//     // The `console.log` is quite polymorphic, so we can bind it with multiple
-//     // signatures. Note that we need to use `js_name` to ensure we always call
-//     // `log` in JS.
-//     #[wasm_bindgen(js_namespace = console, js_name = log)]
-//     fn log_u32(a: u32);
-
-//     #[wasm_bindgen(js_namespace = console, js_name = log)]
-//     fn log_i32(a: i32);
-
-//     // Multiple arguments too!
-//     #[wasm_bindgen(js_namespace = console, js_name = log)]
-//     fn log_many(a: &str, b: &str);
-// }
-
-// static mut rgba_v: Vec<i32> = vec![255; 640*480*4];
 static mut rgba_v: Vec<i32> = Vec::new();
 
 #[wasm_bindgen]
@@ -59,11 +17,6 @@ pub extern fn calculate(rgba: &[i32]){
       }
     }
 
-    // let mut rgba_v = Vec::new();
-    // let mut rgba_v: Vec<i32> = vec![255; 640*480*4];
-    // log_i32(len);
-    // log_i32(rgba_v.len() as i32);
-    // log_u32((640*480*4)+1);
     for x in (0..len).step_by(4) {
       let r = rgba[x as usize];
       let g = rgba[(x+1) as usize];
@@ -87,39 +40,3 @@ pub extern fn calculate(rgba: &[i32]){
 pub extern fn get() -> &[i32] {
   unsafe { &rgba_v[..] }
 }
-
-
-// #[wasm_bindgen]
-// pub fn main(rgba: &mut [i32]){
-//   let len: i32 = rgba.len() as i32;
-//   for x in (0..len).step_by(4) {
-//     let r = rgba[x as usize];
-//     let g = rgba[(x+1) as usize];
-//     let b = rgba[(x+2) as usize];
-//     let average = (r+b+g)/3;
-//     rgba[x as usize] = average;
-//     rgba[(x+1) as usize] = average;
-//     rgba[(x+2) as usize] = average;
-//   }
-// }
-
-// #[wasm_bindgen]
-// pub fn redgreen(rgba: &mut [i32]){
-//   let len: i32 = rgba.len() as i32;
-//   for x in (0..len).step_by(4) {
-//     let r = rgba[x as usize];
-//     let g = rgba[(x+1) as usize];
-//     let b = rgba[(x+2) as usize];
-//     if b > r && b > g  {
-//         rgba[x as usize] = b;
-//         rgba[(x+1) as usize] = g;
-//         rgba[(x+2) as usize] = r;
-//     }
-//     // else {
-//     //     rgba[x as usize] = r;
-//     //     rgba[(x+1) as usize] = g;
-//     //     rgba[(x+2) as usize] = b;
-//     // }
-
-//   }
-// }
